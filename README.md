c96753b47839fb60e93e5148fb65f627b742480a
Laura Wu <leemaiwu@gmail.com>
2023-04-27 19:42:34 -0600

    Completed add recipe page

---
+++ README.md
@@ -0,0 +1,30 @@
+# Devmountain Eatery
+
+Devmountain Eatery showcases a variety of dishes and allows users to access recipes for each dish. The app allows users to click on a specific dish to view its recipe and also provides the option for users to add their own recipes. The app was built using React and bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+![Devmountain Eatery Home]
+
+## How to Use the App
+
+1. Clone or download the project.
+2. Navigate to the directory containing the code and run `npm install` to install all the dependencies.
+3. Run `npm start` to start the app.
+4. Open your browser and navigate to `http://localhost:3000` to use the app.
+
+![Devmountain Eatery Recipe]
+
+## Features
+
+- View different dishes on the home page
+- Click any of the ascociated 'See More' buttons to view specific recipes
+- Add your own recipe by clicking 'Add Recipe'
+
+![Devmountain Eatery Add Recipe Form]
+
+## API
+
+The recipes are displayed from [Devmountain's API](https://recipes.devmountain.com/).
+
+## Author
+
+Laura Wu
--- src/components/newRecipeComponents/NewRecipeScreen.jsx
+++ src/components/newRecipeComponents/NewRecipeScreen.jsx
@@ -1,10 +1,81 @@
-import React from "react"
+import React, { useState } from "react"
+import { Formik } from 'formik'
 
 const NewRecipeScreen = () => {
+  
+  const [ingredients, setIngredients] = useState([])
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState('')
+
+  const initialValues = {
+    type: '',
+    recipeName: '',
+    imageURL: '',
+    prepTime: '',
+    cookTime: '',
+    serves: '',
+    ingredients: [],
+    instructions: ''
+  }
+
+  const ingredientList = ingredients.map((element => {
+    return (
+      <li>
+        {element.quantity} {element.name}
+      </li>
+    )
+  }))
+
   return (
-    <section>
-      <h1>Tell us about your Recipe!</h1>
-      {/* Here you will have a large form. Be prepared, part 4 will have you build this form in detail, and part 5 will have you style it. Do your best! */}
+    <section className="form">
+      <h1 className='add-title'>Tell us about your Recipe!</h1>
+      <Formik>
+        <form>
+          <div className='add-input'>
+            <input type='text' placeholder='Name' className='input-one' />
+            <input type='text' placeholder='Image URL' className='input-one' />
+          </div>
+          <div className='radio-input'>
+            <div className='radio-card'>
+              <input type='radio' value='Cook' name='type' className='input-two' />
+              <h3>Cook</h3>
+            </div>
+            <div className='radio-card'>
+              <input type='radio' value='Bake' name='type' className='input-two' />
+              <h3>Bake</h3>
+            </div>
+            <div className='radio-card'>
+              <input type='radio' value='Drink' name='type' className='input-two' />
+              <h3>Drink</h3>
+          </div>
+          </div>
+          <div className='add-input-three'>
+            <input type='text' placeholder='Prep Time' className='input-three' />
+            <input type='text' placeholder='Cook Time' className='input-three' />
+            <input type='text' placeholder='Serves' className='input-three' />
+          </div>
+          <div className='add-input-four-container'>
+            <div className='add-input-four'>
+              <input type='text' placeholder='Ingredient' className='input-four' onChange={(event) => setName(event.target.value)} />
+              <input type='text' placeholder='Quantity' className='input-four'  onChange={(event) => setQuantity(event.target.value)} />
+            </div>
+            <ul>{ingredientList}</ul>
+          </div>
+          <div className='btn-container'>
+            <button className='add-btn'>
+              Add Another
+            </button>
+          </div>
+          <div className='text-box-container'>
+            <textarea className="text-box" placeholder="What are the instructions?" />
+          </div>
+          <div className="save-container">
+            <button className='save-btn'>
+              Save
+            </button>
+          </div>
+        </form>
+      </Formik>
     </section>
   )
 }
